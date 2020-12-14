03c9e6c93505600fe9b11439158e722bdb406067
Liang Di <iamliangdi@outlook.com>
2020-12-14 20:17:08 +0800

    docs: 添加producer和consumer的yml配置

---
+++ demo-kafka-consumer/src/main/java/com/liangdi/demo/kafka/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.liangdi.demo.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Liang Di
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ demo-kafka-producer/src/main/java/com/liangdi/demo/kafka/producer/ProducerApplication.java
@@ -0,0 +1,15 @@
+package com.liangdi.demo.kafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Liang Di
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ProducerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+}
